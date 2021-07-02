a3d98d00dea0de12c954a36e636e1e3f9b20d81c
fuadnafiz98 <fuadnafiz98@gmail.com>
2021-07-02 17:37:42 +0600

    moved to canary

--- ssg.sh
+++ ssg.sh
@@ -1,94 +1,28 @@
 #!/bin/bash
 
-# folder setup 
+rm -rfd out/**/* 2>/dev/null
+mkdir -p out/metadata
+mkdir -p out/assets
+mkdir -p out/blogs/assets
 
-: '
-rm -rfd blk/**/*
-mkdir -p blk/blogs/_meta
-mkdir -p blk/_assets/
+cp -r template/assets/css out/assets/
 
-cp -r template/_assets/css blk/_assets/
-cp -r template/_assets/js blk/_assets/
+echo "{\"links\": []}" > out/metadata/data.json
 
-totalPages=`ls src/blogs/_meta | wc -l`
-count=0
-
-for file in src/blogs/_meta/*.md; do
-  let count++
-
-  md=$file
-  # echo $md 
-
-  dir=$(dirname $file)
-  dir="${dir:4}" 
-  # basename $file
-  # echo $dir
-
-  # if count == 1 && count == totalPages add nothing
-  # if count == 1 && count < totalPages add next
-  # if count > 1 && count < totalPages add prev & next
-  if [[ $count -eq $totalPages ]] && [[ $count -eq 1 ]]
-  then
-    # add noting
-    # echo pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/pages/blogList.html
-    pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/_blogList.html
-  elif [[ $count -eq $totalPages ]]
-  then
-    # add prev
-    # echo pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/blogList.html -M prevPage="$((count-1))".html
-    pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/_blogList.html -M prevPage="$((count-1))".html
-  elif [[ $count -eq 1 ]] && [[ $count -lt $totalPages ]]
-  then
-    # add next 
-    # echo pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/blogList.html -M nextPage="$((count+1))".html
-    pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/_blogList.html -M nextPage="$((count+1))".html
-  elif [[ $count -gt 1 ]] && [[ $count -lt $totalPages ]]
-  then
-    # add next and prev
-    # echo pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/blogList.html -M prevPage="$((count-1))".html -M nextPage="$((count+1))".html
-    pandoc -s "${md}" -o blk/"${dir}"/"${count}".html --template=template/_pages/_blogList.html -M prevPage="$((count-1))".html -M nextPage="$((count+1))".html
-  fi
-
-done
-
-mkdir -p blk/blogs/contents
-# for file in src/blogs/contents/**/*.md; do
-#   echo $file
-#   dir=$(dirname $file)
-#   dir="${dir:4}" 
-#   mkdir -p blk/$dir
-#   # echo $dir
-#   # echo pandoc -s "${file}" -o blk/"${dir}"/index.html --template=template/pages/blog.html
-#   cp -r src/"${dir}"/assets blk/"${dir}/assets" 2>/dev/null
-#   pandoc -s "${file}" -o blk/"${dir}"/index.html --template=template/_pages/_blog.html
-# done
-
-# cd template/assets/tailwind && yarn tw:dev
-
-'
-
-rm meta/data.json 
-echo "{\"links\": []}" > meta/data.json
-
-for file in src/blogs/contents/**/*.md; do
+for file in src/blogs/**/*.md; do
   echo $file
-  data=$(pandoc --template=./scripts/metadata.pandoc $file | jq)
+  dir=$(dirname $file)
+  dir="${dir:4}"
+  mkdir -p out/$dir
+
+  pandoc -s "${file}" -o out/"${dir}"/index.html --template=template/blog.html
+  cp -r src/"${dir}"/assets/* out/blogs/assets 2>/dev/null
+
+  data=$(pandoc --template=pandoc/metadata.pandoc $file | jq)
   echo $data
-  cat meta/data.json | jq ".links += [$data]" >> meta/d.json && mv meta/d.json meta/data.json 
+
+  cat out/metadata/data.json | jq ".links += [$data]" >> out/metadata/d.json && mv out/metadata/d.json out/metadata/data.json 
 done
 
-# Read data from metadata.json file 
-# $ pandoc -s test.md -o out.html --template=test.html --metadata-file=data.json
-
-# jq -c '.links[]' meta/data.json | while read i;
-# do
-#   # read title thumbnail url < <()
-#   title=$(echo $i | jq -r '.title')
-#   thumbnail=$(echo $i | jq -r '.thumbnail')
-#   url=$(echo $i | jq -r '.url')
-#   # read thumbnail url < <$(echo $i | jq -r '.thumbnail, .url'); dont' work
-#   echo $title;
-#   echo $thumbnail;
-#   echo $url;
-# done
+pandoc -s src/README.md -o out/index.html --template=template/index.html --metadata-file=out/metadata/data.json 
 
